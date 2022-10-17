ac18e5e70afb94705323794ec2fd6824c501abc9
Jēkabs Balodis <signalingpigeon@duck.com>
2022-10-17 10:42:25 +0300

    Nepieciešamie papildinājumi

--- uploadDataGovLV.py
+++ uploadDataGovLV.py
@@ -9,6 +9,7 @@ fileName = stringInput(FILE_NAME_PROMPT)
 
 # TODO atspoguļot informāciju par laiku, kad pēdējo reizi atjaunoti dati
 # TODO atspoguļot informāciju, ka datu augšuplāde bijusi sekmīga
+# TODO https://data.gov.lv/dati/lv/api/3/action/package_show?id=medikamenti-kas-satur-dopinga-vielas
 requests.post('https://data.gov.lv/dati/api/action/resource_update',
               data={'id': resourceID},
               headers={'X-CKAN-API-Key': apiKey},
