b50dbebbe9dfd0f340d2417f86bd2ce7a21d5ba3
Jēkabs Balodis <signalingpigeon@duck.com>
2023-10-23 14:51:58 +0300

    Update ZRApp.py

--- ZRApp.py
+++ ZRApp.py
@@ -4,6 +4,12 @@ from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Role, Permission
 
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
