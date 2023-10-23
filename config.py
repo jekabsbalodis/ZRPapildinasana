bfaae8a527e6f5c776fc949ccf472dbae4dd39f3
Jēkabs Balodis <signalingpigeon@duck.com>
2023-10-23 16:04:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,6 @@ class Config:
     ZRAPP_MAIL_SENDER = 'ZRApp Admin <myhood@inbox.lv>'
     ZRAPP_ADMIN = os.environ.get('ZRAPP_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SSL_REDIRECT = False
     # BOOTSTRAP_BOOTSWATCH_THEME = 'cosmo'
 
     @staticmethod
@@ -36,7 +35,6 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    SSL_REDIRECT = True
 
     @classmethod
     def init_app(cls, app):
@@ -60,13 +58,6 @@ class ProductionConfig(Config):
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-
-        # handle reverse proxy server headers
-        try:
-            from werkzeug.middleware.proxy_fix import ProxyFix
-        except ImportError:
-            from werkzeug.contrib.fixers import ProxyFix
-        app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 config = {
