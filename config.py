9c7ebcf0c4a64ee9e95fc7706fb72c71f95901bf
Jēkabs Balodis <signalingpigeon@duck.com>
2023-10-26 08:32:34 +0000

    Disabling async mail

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ZRAPP_MAIL_SUBJECT_PREFIX = '[ZRApp]'
-    ZRAPP_MAIL_SENDER = 'ZRApp Admin <myhood@inbox.lv>'
+    ZRAPP_MAIL_SENDER = 'ZRApp Admin <zrapp@inbox.lv>'
     ZRAPP_ADMIN = os.environ.get('ZRAPP_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SSL_REDIRECT = False
