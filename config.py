dddcb6d2958628061a4cf5dce5eaf88cd1206e03
jekabsbalodis <signalingpigeon@duck.com>
2023-11-23 16:01:17 +0200

    Editing config.py

--- config.py
+++ config.py
@@ -79,7 +79,7 @@ class PythonAnywhereConfig(ProductionConfig):
 
 class DockerConfig(ProductionConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'db/data-dev.sqlite')
+        'sqlite:///' + os.path.join(basedir, 'db/data.sqlite')
 
     @classmethod
     def init_app(cls, app):
