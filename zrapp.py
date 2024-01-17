3669dbc362a1f9dece6d9ba262a51edfa4a8640e
Jēkabs Balodis <signalingpigeon@duck.com>
2024-01-17 15:10:16 +0000

    Improving code

--- zrapp.py
+++ zrapp.py
@@ -20,7 +20,7 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Permission=Permission)
+    return {'db': db, 'User': User, 'Role': Role, 'Permission': Permission}
 
 
 @app.cli.command()
