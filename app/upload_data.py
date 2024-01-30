0ac8acc737f060276e9f244a8fb9bea5e7a579f5
Jēkabs Balodis <signalingpigeon@duck.com>
2024-01-30 16:54:29 +0200

    Replacing resource_update with resource_patch

--- app/upload_data.py
+++ app/upload_data.py
@@ -6,10 +6,11 @@ import requests
 def upload_data_gov_lv(resource_id, api_key, file_name):
     '''Upload data to data.gov.lv server'''
     with open(file_name, encoding='utf-8') as rb:
-        requests.post('https://data.gov.lv/dati/api/action/resource_update',
-                      data={'id': resource_id},
-                      headers={'X-CKAN-API-Key': api_key},
-                      files=[('upload', rb)])
+        response = requests.post('https://data.gov.lv/dati/api/action/resource_patch',
+                                 data={'id': resource_id},
+                                 headers={'X-CKAN-API-Key': api_key},
+                                 files=[('upload', rb)])
+        return response.json()
 
 
 def upload_zva(user_name, password, ftp_address, ftp_port, file_name):
