db66e5cf310e6c14b2473996af120159b6fe4ba5
Jēkabs Balodis <signalingpigeon@duck.com>
2024-01-30 16:54:48 +0200

    Create unittests

--- app/last_update.py
+++ app/last_update.py
@@ -1,11 +1,11 @@
-'''Record when was the last date
+'''Read when was the last date
 information about recently added
 medications to Drug register was added'''
 from datetime import datetime
 
 
 def last_update():
-    '''Function to record the last data read date'''
+    '''Function to read the last data read date'''
     with open('.lastUpdate', encoding='utf-8') as f:
         update_date = datetime.strptime(f.read(), '%Y-%m-%d').date()
     return update_date
