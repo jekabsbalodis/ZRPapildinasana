029a3f2f2bdfc4d967b89651428f5058f9d05e93
Jēkabs Balodis <signalingpigeon@duck.com>
2024-02-01 09:35:28 +0200

    Create unittests

--- tests/test_upload_data.py
+++ tests/test_upload_data.py
@@ -12,19 +12,19 @@ class UploadDataTestCase(unittest.TestCase):
 
         mock_post.return_value = mock_response
 
-        resource_id = 'ee8f9b14-1eee-494a-b7f5-6777a8232dcb'
-        api_key = '31bf794d-f10c-4dd0-86ff-9ffc9f987505'
+        resource_id = '123'
+        api_key = '123'
         file_name = 'antidopinga_vielas.csv'
 
         result = upload_data_gov_lv(resource_id=resource_id,
                                     api_key=api_key,
                                     file_name=file_name)
 
-        # with open('antidopinga_vielas.csv', encoding='utf-8') as rb:
+        # with open(file_name, encoding='utf-8') as rb:
         #     mock_post.assert_called_with(
         #         'https://data.gov.lv/dati/api/action/resource_patch',
-        #         data={'id': 'ee8f9b14-1eee-494a-b7f5-6777a8232dcb'},
-        #         headers={'X-CKAN-API-Key': '31bf794d-f10c-4dd0-86ff-9ffc9f987505'},
+        #         data={'id': resource_id},
+        #         headers={'X-CKAN-API-Key': api_key},
         #         files=[('upload', rb)])
 
         self.assertEqual(result, mock_response.json.return_value)
